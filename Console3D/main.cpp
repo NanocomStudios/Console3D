86a14efd9809197e6d9d9d27280f87a31b7c6db9
NanocomStudios <dulararupasinghe@gmail.com>
2024-12-16 14:32:23 +0530

    Random Lines

--- Console3D/main.cpp
+++ Console3D/main.cpp
@@ -5,7 +5,37 @@
 
 # define M_PI           3.14159265358979323846 
 
+void drawLine(float x1, float y1, float x2, float y2, char ch);
+
 using namespace std;
+
+class Line {
+private:
+	float pX1 = 0;
+	float pX2 = 0;
+	float pY1 = 0;
+	float pY2 = 0;
+
+public:
+	float X1 = 0;
+	float X2 = 0;
+	float Y1 = 0;
+	float Y2 = 0;
+
+	void draw(char ch) {
+		drawLine(pX1, pY1, pX2, pY2, ' ');
+		drawLine(X1, Y1, X2, Y2, ch);
+		pX1 = X1;
+		pX2 = X2;
+		pY1 = Y1;
+		pY2 = Y2;
+	}
+
+	void erase() {
+		drawLine(pX1, pY1, pX2, pY2, ' ');
+	}
+
+};
 
 void drawLine(float x1, float y1, float x2, float y2, char ch) {
 	int x;
@@ -42,7 +72,8 @@ void drawLine(float x1, float y1, float x2, float y2, char ch) {
 }
 
 void drawLineA(float x, float y, float angle, float length, char ch) {
-	drawLine(x, y, cos(angle) * length, sin(angle) * length, ch);
+	drawLine(x, y, (cos(angle) * length * 2) + x, (sin(angle) * length) + y, ch);
+	//cout << cos(angle) * length << '\t' << sin(angle) * length << endl;
 	
 }
 
@@ -50,15 +81,23 @@ double toRad(double angle) {
 	return (angle * M_PI) / 180;
 }
 
+void drawRandomLine() {
+	consoleColorSet(((rand() % 2) == 0) ? ((rand() % 8) + 40) : ((rand() % 8) + 100));
+	drawLine((rand() % 80), (rand() % 25), (rand() % 80), (rand() % 25), ' ');
+}
+
 int main() {
-	int previousI = 0;
+	srand(time(0));
+	Line line1;
 	while (1) {
-		for (int i = 0; i < 360; i++) {
-			drawLineA(40, 25, toRad(previousI), 5, ' ');
-			drawLineA(40, 25, toRad(i), 5, '0');
-			previousI = i;
-			Sleep(50);
-		}
+		
+		line1.X1 = (rand() % 80);
+		line1.X2 = (rand() % 80);
+
+		line1.Y1 = (rand() % 25);
+		line1.Y2 = (rand() % 25);
+		line1.draw('H');
+		Sleep(100);
 	}
 }
 
